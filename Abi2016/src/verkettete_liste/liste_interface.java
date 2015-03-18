11b04895577880af92ff863403af9865f023a236
leon-schule <leon-schule@172.16.240.81>
2015-03-18 12:39:16 +0100

    Added Interface for Chained List in Java.

--- Abi2016/src/verkettete_liste/liste_interface.java
+++ Abi2016/src/verkettete_liste/liste_interface.java
@@ -1,14 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package verkettete_liste;
 
-/**
- *
- * @author leon-schule
- */
 public interface liste_interface {
+    
+    
     
 }
