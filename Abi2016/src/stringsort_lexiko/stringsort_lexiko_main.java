3c45e1d69a4b802061f136d387c2c43b31a1d668
leon-schule <leon-schule@172.16.240.51>
2015-04-23 13:45:12 +0200

    Finished String Sorting.

--- Abi2016/src/stringsort_lexiko/stringsort_lexiko_main.java
+++ Abi2016/src/stringsort_lexiko/stringsort_lexiko_main.java
@@ -1,16 +1,41 @@
 package stringsort_lexiko;
 
-import verkettete_liste_doppelt.*;
+import verkettete_liste.*;
 
 public class stringsort_lexiko_main {
 
     static String[] data = {"Bauausführung", "Steuer", "Staatsbesitz", "Theologe", "Museumsinsel", "Kulturbesitz", "Ortschaft", "Zukunft"};
-    static liste_doppelt mainList = new liste_doppelt();
+    static liste mainList = new liste();
     static stringcompare stg = new stringcompare();
 
     public static void main(String[] args) {
 
+        mainList.append(data[0]);
+        mainList.toFirst();
         
+        for (int i = 1; i < data.length; i++) {
+            
+            mainList.toFirst();
+            
+            while((mainList.getObject() != null) && stg.compareStrings((String)mainList.getObject(), data[i]) == 0) {
+                
+                mainList.next();
+                
+            }
+            
+            if (mainList.getObject() != null) {
+                mainList.insert(data[i]);
+            } else {
+                mainList.append(data[i]);
+            }
+            
+        }
+        
+        mainList.toFirst();
+        while(mainList.getObject() != null) {
+            System.out.println(mainList.getObject());
+            mainList.next();
+        }
         
     }
 
